677b253d0585dd84b24de1283a3151052aefd102
Gaelen Braithwait <gaelenb0720@gmail.com>
2015-12-13 13:22:47 -0600

    Init QuoteGen skeleton structure

---
+++ scripts/jquery.script.js
@@ -0,0 +1,75 @@
+
+
+
+(function() {
+	"use strict";
+
+
+var model = {
+		
+	};
+
+
+
+
+
+
+
+
+var controller = {
+	
+	
+	
+};
+
+
+
+var view = {
+
+	
+
+};
+
+
+// On page load init should call controller to get initial
+//value of break and work times and send them to the model
+//
+//
+
+var init = function() {
+	
+	
+};
+
+$(document).ready(function () {
+
+
+
+
+});
+
+// 
+
+//Controls 
+	//for timer
+
+$('.new-button').click(function() {
+	
+	view.started();
+});
+
+
+$('.tweet-quote').click(function() {
+	view.reset();
+});
+
+	//for audio
+
+
+
+
+
+})();
+
+
+// aPJztPTRfYmshMT0OPwGBRz2016up1ghQyIjsn9T7NV3hcjtIt
