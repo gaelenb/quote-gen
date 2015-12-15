64d74eeabc1ebc596fbffe74baaf5e7a4e1af3c3
Gaelen Braithwait <gaelenb0720@gmail.com>
2015-12-15 01:05:57 -0600

    Left off with issue chaining events to ajax request and twitter script loading

--- scripts/jquery.script.js
+++ scripts/jquery.script.js
@@ -6,7 +6,7 @@
 
 
 var model = {
-		quoteData: []
+		
 	};
 
 
@@ -26,10 +26,15 @@ var controller =
 		    dataType: 'json',
 		    success: function(data) {
 		    	 model = {
-		    	 	quote: data
+		    	 	quote: data.quote,
+		    	 	//cache shortened string value for creating a new share button
+		    	 	stringForTweet: data.author
 		    	 			
 		    	 };
 		    	 view.showQuote(model.quote);
+		    	 
+		    	 console.log(data.quote);
+		    	 
 		    },
 		    error: function(err) { alert(err); },
 		    beforeSend: function(xhr) {
@@ -39,8 +44,15 @@ var controller =
 	},
 
 	formatQuote: function formatQuote(data) {
-		
+		var string = data;
+		var length = 140;
+		var trimmedString = string.length > length ? 
+                    string.substring(0, length - 3) + "..." : 
+                    string.substring(0, length);
+		console.log(trimmedString);
+		return trimmedString;
 	}
+
 };
 
 	
@@ -54,17 +66,32 @@ var view = {
 	showQuote: function showQuote(data) {
 		var quote = data;
 
-
-		for (var text in quote) {
-			
-			$(".quote-container").append("<div class='text'>" +text +": " + quote[text] + "</div>");
-
-		}
+		$('.quote-container').html(
+			"<div class='text quote'>"+quote.quote + "</div>" + "<div class='text author'>~" + quote.author + "</div>");
 		
-	}
+		//triggers creation of new share button
+		
+	},
+	shareButton : function shareButton(textData) {
+		var quoteText = textData;
+		twttr.widgets.createShareButton(
+			'',	
+	  document.getElementById('new-tweet'),
+	  {
+	    count: 'none',
+	    text: quoteText,
+	    size: 'large'
+	  }).then(function (el) {
+	    console.log("Button created.");
+	  });
+},
+
+	twitInit : function twitInit() {
+		
+		}
 
 };
-
+$(".new-tweet").attr('id', 'new-tweet');
 
 
 var init = function() {
@@ -74,33 +101,81 @@ var init = function() {
 
 $(document).ready(function () {
 
-console.log(controller);
+var defer = $.Deferred();
+
+var x= defer.then(function(value) {
+	
+	var newString;
+	var quote = controller.formatQuote(value);
+	// var author = controller.formatQuote(val);
+
+	return quote; 
+});
+
+
+//NEED TO USE THESE CHAINS ON THE AJAX REQUEST-- twitter script
+//will be done by then.? can use new knoweldge of chains to 
+//load twitter script later. . . ?
+
+
+
+
+defer.resolve(model.quote);
+
+x.done(function(value) {
+	view.shareButton(value);
+});
+
+// controller.requestQuote();
+
+twttr.ready(
+	function(twttr) {
+		twttr.events.bind(
+  'loaded',
+  function (event) {
+    event.widgets.forEach(function (widget) {
+      console.log("Created widget", widget.id);
+    });
+  }
+);
+		// twttr.widgets.load(document.getElementById('new-tweet'));
+		
+	});
+
 
 
 });
 
-// 
 
 //Controls 
 	//for timer
 
 $('.quote-button').click(function() {
 	
-	controller.requestQuote();
+	var tweetButton = $("iframe[id*='twitter']");
+	if(tweetButton) {
+		tweetButton.remove();
+	} 
+	// controller.requestQuote();
+	view.shareButton();
+	// view.shareButton(model.quote);
+	// controller.requestQuote();
 });
 
 
-$('.tweet-quote').click(function() {
-	view.reset();
-});
-
-	//for audio
 
 
 
 
 
 })();
+
+
+
+
+
+
+
 
 
 // 
